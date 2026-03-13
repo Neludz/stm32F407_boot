2537c3ff5cbbcee5c54d8e8776e906a7dc6dab6f
Литвин Алексей <a.litvin@pluton.local>
2026-03-13 16:26:25 +0300

    Signed-off-by: Литвин Алексей <a.litvin@pluton.local>

---
+++ inc/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32f4xx.h"
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MAIN_H_INCLUDED */
