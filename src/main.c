2be68619b8161ad5f6bb3a65ad752fbf283bc129
Литвин Алексей <a.litvin@pluton.local>
2026-03-13 16:58:50 +0300

    Signed-off-by: Литвин Алексей <a.litvin@pluton.local>

--- src/main.c
+++ src/main.c
@@ -4,9 +4,6 @@ extern  uint32_t _user_app_addr;
 uint32_t user_app_addr = (uint32_t) &_user_app_addr;
 int main(void)
 {
-
-
-
 	uint32_t appJumpAddress;
 	void (*GoToApp)(void);
 	appJumpAddress = *((volatile uint32_t*)(user_app_addr + 4));
@@ -14,8 +11,5 @@ int main(void)
 	SCB->VTOR = (uint32_t) user_app_addr;
 	__set_MSP(*((volatile uint32_t*)  user_app_addr)); //stack pointer (to RAM) for USER app in this address
 	GoToApp();
-
-
-
 }
 
